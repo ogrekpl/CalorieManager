c3f4d324aea96d50055b11de9fe7f738e6ee8d77
Sebastian Marciniak <sebastian.marciniak@gmail.com>
2020-12-07 17:20:58 +0100

    Base classes for User and Activity
    New classes:
    User.cs
    Activity.cs

---
+++ CalorieManager/CalorieManager/Classes/Activity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieManager
+{
+	class Activity
+	{
+		private uint id;
+		private string name;
+		private string description;
+		private int calories;
+		public uint Id => id;
+		public string Name => name;
+		public string Description => description;
+		public int Calories => calories;
+
+		/// <summary>
+		/// Constructor of Activity class
+		/// </summary>
+		/// <param name="id">Id</param>
+		/// <param name="name">Name</param>
+		/// <param name="description">Description</param>
+		/// <param name="calories">Calories</param>
+		public Activity(uint id, string name, string description, int calories)
+		{
+			this.id = id;
+			this.name = name;
+			this.description = description;
+			this.calories = calories;
+		}
+	}
+}
---
+++ CalorieManager/CalorieManager/Classes/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieManager
+{
+	class User
+	{
+		private uint id;
+		private string name;
+		private int age;
+		private int height;
+		private int caloriesGoal;
+		private double weightGoal;
+		private Dictionary<DateTime, double> weightHistory;
+		public uint Id => id;
+		public string Name => name;
+		public int Age => age;
+		public int Height => height;
+		public int CaloriesGoal => caloriesGoal;
+		public double WeightGoal => weightGoal;
+		public Dictionary<DateTime, double> WeightHistory => weightHistory;
+
+
+		/// <summary>
+		/// Contructor of class User
+		/// </summary>
+		/// <param name="id">Id</param>
+		/// <param name="name">Name</param>
+		/// <param name="age">Age</param>
+		/// <param name="height">Height(cm)</param>
+		/// <param name="caloriesGoal">Calories Goal</param>
+		/// <param name="weightGoal">Weight goal</param>
+		/// <param name="weightHistory">Weight History</param>
+		public User(uint id, string name, int age, int height, int caloriesGoal, double weightGoal, Dictionary<DateTime, double> weightHistory)
+		{
+			this.id = id;
+			this.name = name;
+			this.age = age;
+			this.height = height;
+			this.caloriesGoal = caloriesGoal;
+			this.weightGoal = weightGoal;
+			this.weightHistory = weightHistory;
+		}
+
+	}
+}
